2972054b4093e289c39be9ec3846dee99c139810
elbek <elbekmirzamakhmudov@gmail.com>
2024-10-11 00:45:45 +0500

    bug fixes

--- lib/fcm_sending.dart
+++ lib/fcm_sending.dart
@@ -2,10 +2,10 @@ library fcm_sending;
 
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:fcm_sending/fcm_message.dart';
 import 'package:flutter/material.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class FcmSending {
@@ -13,15 +13,17 @@ class FcmSending {
 
   static Future<void> sendMessage(FcmMessage message) async {
     final bearerToken = await _getBearerToken();
+    Dio dio = Dio();
+
     try {
-      post(
-        Uri.parse(
-          "https://fcm.googleapis.com/v1/projects/${_pref.getString('projectId')}/messages:send",
+      dio.post(
+        "https://fcm.googleapis.com/v1/projects/${_pref.getString('projectId')}/messages:send",
+        data: message.toJson(),
+        options: Options(
+          headers: {
+            "Authorization": "Bearer $bearerToken",
+          },
         ),
-        body: message.toJson(),
-        headers: {
-          "Authorization": "Bearer $bearerToken",
-        },
       );
     } catch (e, s) {
       debugPrint("$e \n$s");
@@ -45,7 +47,6 @@ class FcmSending {
   static init({required Map<String, dynamic> serviceAccountCredentials}) async {
     _pref = await SharedPreferences.getInstance();
     _pref.setString("serviceAccount", jsonEncode(serviceAccountCredentials));
-    _pref.setString(
-        "projectId", jsonEncode(serviceAccountCredentials['project_id']));
+    _pref.setString("projectId", serviceAccountCredentials['project_id']);
   }
 }
